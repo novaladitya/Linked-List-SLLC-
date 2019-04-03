a86c8745d2fb69fbc7e2a4f0975fad50f17cf1dd
MFajru <48635167+MFajru@users.noreply.github.com>
2019-04-03 23:56:10 +0700

    Update Utama.cpp
    Pembetukan SLLC head and tail

--- Utama.cpp
+++ Utama.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 using namespace std;
+
+struct nodeLinkedList{
+	int data;
+	nodeLinkedList *next;
+};
+
+nodeLinkedList *head, *tail;
+
+void operasiLinkedList(){
+	head = NULL;
+	tail = NULL;
+}
+		
+int tailKosong(){
+	if(tail == NULL)
+		return 1;
+	else
+		return 0;
+}
 int main(){
 	return 0;
 //oke
